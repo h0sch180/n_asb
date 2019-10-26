c7321d2aaaff60ed2eea888a0472c6af5747a935
h0sch180 <hoschi@anukis.de>
2019-10-26 23:09:07 +0200

    fix framework base 05-10-2019

--- crdroid/n_asb_2019-10.sh
+++ crdroid/n_asb_2019-10.sh
@@ -4,12 +4,9 @@ cd ../../../../../..
 cd build
 git fetch "https://github.com/LineageOS/android_build" refs/changes/49/260549/1 && git cherry-pick FETCH_HEAD
 cd ..
-#cd bionic
-#git fetch "https://github.com/LineageOS/android_bionic" refs/changes/60/260560/2 && git cherry-pick FETCH_HEAD
-#cd ..
-#cd external/icu
-#git fetch "https://github.com/LineageOS/android_external_icu" refs/changes/61/260561/1 && git cherry-pick FETCH_HEAD
-#cd ../..
+cd frameworks/base
+git fetch "https://github.com/LineageOS/android_frameworks_base" refs/changes/50/260550/1 && git cherry-pick FETCH_HEAD
+cd ../..
 #cd frameworks/av
 #git fetch "https://github.com/LineageOS/android_frameworks_av" refs/changes/52/260552/1 && git cherry-pick FETCH_HEAD
 #git fetch "https://github.com/LineageOS/android_frameworks_av" refs/changes/51/260551/1 && git cherry-pick FETCH_HEAD
