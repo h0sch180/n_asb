b84646a53a09ddefacab2e83807613a301cb3bd5
h0sch180 <hoschi@anukis.de>
2019-12-24 12:13:55 +0000

    n_asb 2019.12.05

--- crdroid/apply-patches.sh
+++ crdroid/apply-patches.sh
@@ -54,10 +54,13 @@ bash n_asb_2019-10.sh
 cd $CURRENT_DIR
 bash n_asb_2019-11.sh
 
+cd $CURRENT_DIR
+bash n_asb_2019-12.sh
+
 sleep 1
 echo -e "\n all patches are included now"
 sleep 2
-echo -e "\n bump security patch level to 2019-11-05"
+echo -e "\n bump security patch level to 2019-12-05"
 echo -e "\n script by seluce (until 2019-08-05)"
 echo -e "\n script by h0sch180 + seluce (2019-09-05 until now)"
 
