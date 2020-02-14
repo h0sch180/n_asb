f191ccfe6f75632a9ca3ae0760f33fc0474cd37b
h0sch180 <hoschi@anukis.de>
2020-02-14 20:52:20 +0000

    fix n-asb-2020-02

--- crdroid/n_asb_2020-02.sh
+++ crdroid/n_asb_2020-02.sh
@@ -15,15 +15,15 @@ cd ../..
 cd frameworks/base
 git fetch "https://github.com/LineageOS/android_frameworks_base" refs/changes/63/268263/1 && git cherry-pick FETCH_HEAD
 git fetch "https://github.com/LineageOS/android_frameworks_base" refs/changes/64/268264/1 && git cherry-pick FETCH_HEAD
-git fetch "https://github.com/LineageOS/android_frameworks_base" refs/changes/65/268265/1 && git cherry-pick FETCH_HEAD
+#git fetch "https://github.com/LineageOS/android_frameworks_base" refs/changes/65/268265/1 && git cherry-pick FETCH_HEAD
 cd ../..
-cd packages/apps/Settings
-git fetch "https://github.com/LineageOS/android_packages_apps_Settings" refs/changes/59/268259/1 && git cherry-pick FETCH_HEAD
-cd ../../..
+#cd packages/apps/Settings
+#git fetch "https://github.com/LineageOS/android_packages_apps_Settings" refs/changes/59/268259/1 && git cherry-pick FETCH_HEAD
+#cd ../../..
 #cd packages/apps/CertInstaller
 #git fetch "https://github.com/LineageOS/android_packages_apps_CertInstaller" refs/changes/58/268258/1 && git cherry-pick FETCH_HEAD
 #cd ../../..
-cd external/sqlite
-git fetch "https://github.com/LineageOS/android_external_sqlite" refs/changes/57/268257/1 && git cherry-pick FETCH_HEAD
-cd ../..
+#cd external/sqlite
+#git fetch "https://github.com/LineageOS/android_external_sqlite" refs/changes/57/268257/1 && git cherry-pick FETCH_HEAD
+#cd ../..
 
--- resurrectionremix/n_asb_2020-02.sh
+++ resurrectionremix/n_asb_2020-02.sh
@@ -12,14 +12,14 @@ cd ../..
 #git fetch "https://github.com/LineageOS/android_system_bt" refs/changes/61/268261/1 && git cherry-pick FETCH_HEAD
 #git fetch "https://github.com/LineageOS/android_system_bt" refs/changes/62/268262/1 && git cherry-pick FETCH_HEAD
 #cd ../..
-cd frameworks/base
-git fetch "https://github.com/LineageOS/android_frameworks_base" refs/changes/63/268263/1 && git cherry-pick FETCH_HEAD
-git fetch "https://github.com/LineageOS/android_frameworks_base" refs/changes/64/268264/1 && git cherry-pick FETCH_HEAD
-git fetch "https://github.com/LineageOS/android_frameworks_base" refs/changes/65/268265/1 && git cherry-pick FETCH_HEAD
-cd ../..
-cd packages/apps/Settings
-git fetch "https://github.com/LineageOS/android_packages_apps_Settings" refs/changes/59/268259/1 && git cherry-pick FETCH_HEAD
-cd ../../..
+#cd frameworks/base
+#git fetch "https://github.com/LineageOS/android_frameworks_base" refs/changes/63/268263/1 && git cherry-pick FETCH_HEAD
+#git fetch "https://github.com/LineageOS/android_frameworks_base" refs/changes/64/268264/1 && git cherry-pick FETCH_HEAD
+#git fetch "https://github.com/LineageOS/android_frameworks_base" refs/changes/65/268265/1 && git cherry-pick FETCH_HEAD
+#cd ../..
+#cd packages/apps/Settings
+#git fetch "https://github.com/LineageOS/android_packages_apps_Settings" refs/changes/59/268259/1 && git cherry-pick FETCH_HEAD
+#cd ../../..
 #cd packages/apps/CertInstaller
 #git fetch "https://github.com/LineageOS/android_packages_apps_CertInstaller" refs/changes/58/268258/1 && git cherry-pick FETCH_HEAD
 #cd ../../..
