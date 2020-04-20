252d22506dff04af81acd970238f9a44602969ca
h0sch180 <hoschi@anukis.de>
2020-04-20 14:06:46 +0000

    update

--- viperosextended/n_asb_2020-03.sh
+++ viperosextended/n_asb_2020-03.sh
@@ -8,9 +8,9 @@ git fetch "https://github.com/LineageOS/android_frameworks_base" refs/changes/20
 git fetch "https://github.com/LineageOS/android_frameworks_base" refs/changes/21/270221/1 && git cherry-pick FETCH_HEAD
 git fetch "https://github.com/LineageOS/android_frameworks_base" refs/changes/22/270222/1 && git cherry-pick FETCH_HEAD
 cd ../..
-cd packages/providers/TelephonyProvider
-git fetch "https://github.com/LineageOS/android_packages_providers_TelephonyProvider" refs/changes/23/270223/1 && git cherry-pick FETCH_HEAD
-cd ../../..
+#cd packages/providers/TelephonyProvider
+#git fetch "https://github.com/LineageOS/android_packages_providers_TelephonyProvider" refs/changes/23/270223/1 && git cherry-pick FETCH_HEAD
+#cd ../../..
 #cd external/libvpx
 #git fetch "https://github.com/LineageOS/android_external_libvpx" refs/changes/19/270219/1 && git cherry-pick FETCH_HEAD
 #cd ../..
