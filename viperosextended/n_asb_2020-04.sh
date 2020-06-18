4bbf459dac420399429f3319a83c9cf15dbcdeda
h0sch180 <hoschi@anukis.de>
2020-06-18 19:54:43 +0000

    update

--- viperosextended/n_asb_2020-04.sh
+++ viperosextended/n_asb_2020-04.sh
@@ -7,10 +7,10 @@ cd frameworks/base
 git fetch "https://github.com/LineageOS/android_frameworks_base" refs/changes/55/272355/1 && git cherry-pick FETCH_HEAD
 git fetch "https://github.com/LineageOS/android_frameworks_base" refs/changes/56/272356/1 && git cherry-pick FETCH_HEAD
 cd ../..
-cd external/sqlite
-git fetch "https://github.com/LineageOS/android_external_sqlite" refs/changes/03/268803/2 && git cherry-pick FETCH_HEAD
-git fetch "https://github.com/LineageOS/android_external_sqlite" refs/changes/04/268804/2 && git cherry-pick FETCH_HEAD
-cd ../..
+#cd external/sqlite
+#git fetch "https://github.com/LineageOS/android_external_sqlite" refs/changes/03/268803/2 && git cherry-pick FETCH_HEAD
+#git fetch "https://github.com/LineageOS/android_external_sqlite" refs/changes/04/268804/2 && git cherry-pick FETCH_HEAD
+#cd ../..
 #cd external/libnfc-nci
 #git fetch "https://github.com/LineageOS/android_external_libnfc-nci" refs/changes/53/272353/3 && git cherry-pick FETCH_HEAD
 #git fetch "https://github.com/LineageOS/android_external_libnfc-nci" refs/changes/54/272354/3 && git cherry-pick FETCH_HEAD
