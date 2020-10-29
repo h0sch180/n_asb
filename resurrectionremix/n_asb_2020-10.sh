d21c845898cf12988579fca32669e3ab20ab4a55
h0sch180 <hoschi@anukis.de>
2020-10-29 21:49:36 +0000

    fix 2

--- resurrectionremix/n_asb_2020-10.sh
+++ resurrectionremix/n_asb_2020-10.sh
@@ -3,16 +3,16 @@ cd ../..
 cd build
 git fetch "https://github.com/LineageOS/android_build" refs/changes/87/288787/2 && git cherry-pick FETCH_HEAD
 cd ..
-cd packages/apps/Bluetooth
-git fetch "https://github.com/LineageOS/android_packages_apps_Bluetooth" refs/changes/33/285933/2 && git cherry-pick FETCH_HEAD
-cd ../../..
+#cd packages/apps/Bluetooth
+#git fetch "https://github.com/LineageOS/android_packages_apps_Bluetooth" refs/changes/33/285933/2 && git cherry-pick FETCH_HEAD
+#cd ../../..
 #cd packages/apps/Nfc
 #git fetch "https://github.com/LineageOS/android_packages_apps_Nfc" refs/changes/90/288790/1 && git cherry-pick FETCH_HEAD
 #cd ../../..
 #cd system/bt
 #git fetch "https://github.com/LineageOS/android_system_bt" refs/changes/27/285927/6 && git cherry-pick FETCH_HEAD
 #cd ../..
-cd system/bt
+cd system/core
 git fetch "https://github.com/LineageOS/android_system_core" refs/changes/01/286001/3 && git cherry-pick FETCH_HEAD
 git fetch "https://github.com/LineageOS/android_system_core" refs/changes/00/286000/3 && git cherry-pick FETCH_HEAD
 cd ../..
